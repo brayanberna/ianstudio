219e154f132f36fa7a7b056d8e7408e32d4eab13
Brayan <brayan.057@live.cl>
2022-01-08 03:28:31 -0300

    primer deploy43

--- ian_analytics/settings/base.py
+++ ian_analytics/settings/base.py
@@ -106,7 +106,8 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/build/static'),
+    #os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, '../frontend/build/static'),
 ]
 
 CORS_ORIGIN_WHITELIST = [
@@ -115,8 +116,7 @@ CORS_ORIGIN_WHITELIST = [
     #"https://ianstudio.herokuapp.com/",
 ]
 
-#MEDIA_URL = 'frontend/public/files/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/public/files/')
 
 MEDIA_URL = '/files/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/public/files')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/public/files')
+MEDIA_ROOT = os.path.join(BASE_DIR, '../frontend/public/files')
