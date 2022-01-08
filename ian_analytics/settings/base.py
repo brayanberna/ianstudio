2fcc30ff377ae265a2369ed8529b0b9a5a34a1c7
Brayan <brayan.057@live.cl>
2022-01-08 02:51:33 -0300

    primer deploy42

--- ian_analytics/settings/base.py
+++ ian_analytics/settings/base.py
@@ -44,7 +44,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/build')
+            #os.path.join(BASE_DIR, 'frontend/build')
+            os.path.join(BASE_DIR, 'frontend/public')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
