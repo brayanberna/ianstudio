f95b04665f1586efd44012887050d839a70eae50
Brayan <brayan.057@live.cl>
2022-01-08 04:47:36 -0300

    primer deploy60

--- ian_analytics/asgi.py
+++ ian_analytics/asgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ian_analytics.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ian_analytics.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ian_analytics.settings.production')
 
-application = get_asgi_application()
+#application = get_asgi_application()
+
+from dj_static import Cling
+application = Cling(get_wsgi_application())
