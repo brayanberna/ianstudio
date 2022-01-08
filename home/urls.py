19c1a2323ed0655e1b9cc3a9fec2fcaf60b1247c
Brayan <brayan.057@live.cl>
2022-01-08 08:40:54 -0300

    primer deploy96

--- home/urls.py
+++ home/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
   path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
   path('detect_type_neuronal/', views.detect_type_neuronal, name='detect_type_neuronal'),
   path('run_red_neuronal/', views.run_red_neuronal, name='run_red_neuronal'),
-  path('load_data/', views.load_data, name='load_data'),
+  path('https://ianstudio.herokuapp.com/load_data/', views.load_data, name='load_data'),
   path('consult_red_neuronal/', views.consult_red_neuronal, name='consult_red_neuronal'),
 ]
