408d23bde8a3798971489157e5de9167a71c90f0
Brayan <brayan.057@live.cl>
2022-01-10 04:58:28 -0300

    primer deploy114

--- ian_analytics/settings/base.py
+++ ian_analytics/settings/base.py
@@ -114,7 +114,7 @@ STATICFILES_DIRS = [
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:3000",
-    "http://127.0.0.1:8000"
+    #"ianstudio.herokuapp.com",
     "https://ianstudio.herokuapp.com",
 ]
 
