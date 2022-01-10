153e84c5d631671a9677e46f65c1ed293af17c5a
Brayan <brayan.057@live.cl>
2022-01-10 04:01:29 -0300

    primer deploy111

--- ian_analytics/settings/production.py
+++ ian_analytics/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ianstudio.herokuapp.com', '127.0.0.1:8000']
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
